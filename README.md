2e946c28b86e58326831ef6861709295cf97abc5
Przemysław Pietras <przemyslawp94@gmail.com>
2018-10-12 00:19:14 +0200

    Add better README

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
+# at-date
+
 [![Build Status](https://travis-ci.org/destag/at-date.svg?branch=master)](https://travis-ci.org/destag/at-date)
 [![codecov](https://codecov.io/gh/destag/at-date/branch/master/graph/badge.svg)](https://codecov.io/gh/destag/at-date)
 [![CodeFactor](https://www.codefactor.io/repository/github/destag/at-date/badge)](https://www.codefactor.io/repository/github/destag/at-date)
 [![PyPI version](https://badge.fury.io/py/atdate.svg)](https://badge.fury.io/py/atdate)
 [![PyPI pyversions](https://img.shields.io/pypi/pyversions/atdate.svg)](https://pypi.python.org/pypi/atdate/)
 
-# at date
+Simple Python library for parsing **at command** string into datetime objects.
+
+```
+noon next day -> 2018-10-11 12:00:00
+```
+
+## Installation
+
+To install **at-date** simply use pip:
+
+```bash
+pip install atdate
+```
+
+## Getting started
+
+To use **at-date** simply import **parse** function.
+
+```python
+>>> from atdate import parse
+
+>>> parse('noon next day')
+datetime.datetime(2018, 10, 11, 12, 0)
+
+>>> parse('now + 8 hours')
+datetime.datetime(2018, 10, 10, 15, 42, 24)
+```
+
+More info can be found in **/docs** directory or in [docs](#) page.
+
+## How to Contribute
+
+Take a look at [CONTRIBUTING](CONTRIBUTING.md) guide.
+
+## License
+
+**At-date** is licensed under MIT License. See [LICENSE](LICENSE) for more information.
--- setup.py
+++ setup.py
@@ -5,27 +5,27 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='atdate',
-    version='0.0.1-dev10',
+    version='0.0.1-dev11',
     author='Przemysław Pietras',
     author_email='przemyslawp94@gmail.com',
     description='Simple linux at command string parser',
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords=[
-        'atdate'
+        'atdate',
         'at',
         'date',
         'datetime',
         'time',
         'parse',
-        'parser'
+        'parser',
     ],
     url='https://github.com/destag/at-date',
-    license='MIT License'
-    packages=setuptools.find_packages(exclude=['test']),
+    license='MIT License',
+    packages=setuptools.find_packages(exclude=['test', 'docs']),
     install_requires=[
         'lark-parser >= 0.6.3',
-        'python-dateutil >= 2.7.3'
+        'python-dateutil >= 2.7.3',
     ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
@@ -33,6 +33,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Operating System :: OS Independent',
         'Topic :: Software Development',
