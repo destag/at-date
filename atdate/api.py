73661770c3e1c9eb6d31baca2d80f5c52ae4cdc4
Przemysław Pietras <przemyslawp94@gmail.com>
2021-10-01 11:39:43 +0200

    chore: add typing (#108)

--- atdate/api.py
+++ atdate/api.py
@@ -5,14 +5,14 @@ from dateutil.relativedelta import relativedelta
 
 from .atdate_format import format_string
 
-from typing import Optional
+from typing import Optional, List
 
 
 class AtDateParser:
-    def __init__(self):
+    def __init__(self) -> None:
         self.parser = Lark(format_string, start="timespec")
 
-    def execute(self, string_to_parse:str) -> Optional[datetime]:
+    def execute(self, string_to_parse: str) -> Optional[datetime]:
         transformer = AtDateTransformer()
         tree = self.parser.parse(string_to_parse.lower())
         new_tree = transformer.transform(tree)
@@ -27,7 +27,7 @@ class AtDateParser:
         return next_time_run
 
 
-class AtDateTransformer(Transformer):
+class AtDateTransformer(Transformer):  # type: ignore
     _month_name_to_month_number = {
         "jan": 1,
         "january": 1,
@@ -54,7 +54,7 @@ class AtDateTransformer(Transformer):
         "december": 12,
     }
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.now = datetime.now()
         self.datetime_params = {
@@ -67,10 +67,22 @@ class AtDateTransformer(Transformer):
             "microsecond": 0,
         }
 
-    def _now(self, matches):
+    @property
+    def date_time(self) -> datetime:
+        return datetime(
+            year=self.datetime_params["year"],
+            month=self.datetime_params["month"],
+            day=self.datetime_params["day"],
+            hour=self.datetime_params["hour"],
+            minute=self.datetime_params["minute"],
+            second=self.datetime_params["second"],
+            microsecond=self.datetime_params["microsecond"],
+        )
+
+    def _now(self, matches: List[str]) -> datetime:
         return self.now
 
-    def _hr24clock_hr_min(self, matches):
+    def _hr24clock_hr_min(self, matches: List[str]) -> datetime:
         hour = int(matches[0][:2])
         minute = int(matches[0][2:] or 0)
         next_day = self._check_if_next_day(hour, minute)
@@ -78,9 +90,9 @@ class AtDateTransformer(Transformer):
         self.datetime_params["hour"] = hour
         self.datetime_params["minute"] = minute
         self.datetime_params["second"] = 0
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _iso_time(self, matches):
+    def _iso_time(self, matches: List[str]) -> datetime:
         hour = int(matches[0])
         minute = int(matches[1])
         next_day = self._check_if_next_day(hour, minute)
@@ -88,9 +100,9 @@ class AtDateTransformer(Transformer):
         self.datetime_params["hour"] = hour
         self.datetime_params["minute"] = minute
         self.datetime_params["second"] = 0
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _wallclock_hr_min_am_pm(self, matches):
+    def _wallclock_hr_min_am_pm(self, matches: List[str]) -> datetime:
         am_pm = matches[1]
         hour = int(matches[0][:2]) % 12 + 12 * int(am_pm == "pm")
         minute = int(matches[0][2:] or 0)
@@ -99,9 +111,9 @@ class AtDateTransformer(Transformer):
         self.datetime_params["hour"] = hour
         self.datetime_params["minute"] = minute
         self.datetime_params["second"] = 0
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _wallclock_hour_minute_am_pm(self, matches):
+    def _wallclock_hour_minute_am_pm(self, matches: List[str]) -> datetime:
         am_pm = matches[2]
         hour = int(matches[0]) % 12 + 12 * int(am_pm == "pm")
         minute = int(matches[1])
@@ -110,95 +122,99 @@ class AtDateTransformer(Transformer):
         self.datetime_params["hour"] = hour
         self.datetime_params["minute"] = minute
         self.datetime_params["second"] = 0
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _noon(self, matches):
+    def _noon(self, matches: List[str]) -> datetime:
         next_day = timedelta(days=self._check_if_next_day(12, 0))
         self.datetime_params["hour"] = 12
         self.datetime_params["minute"] = 0
         self.datetime_params["second"] = 0
-        dt = datetime(**self.datetime_params)
+        dt = self.date_time
         return dt + next_day
 
-    def _midnight(self, matches):
+    def _midnight(self, matches: List[str]) -> datetime:
         next_day = timedelta(days=1)
         self.datetime_params["hour"] = 0
         self.datetime_params["minute"] = 0
         self.datetime_params["second"] = 0
-        dt = datetime(**self.datetime_params)
+        dt = self.date_time
         return dt + next_day
 
-    def _month_name_day_number(self, matches):
+    def _month_name_day_number(self, matches: List[str]) -> datetime:
         month = self.__class__._month_name_to_month_number[matches[0]]
         day = int(matches[1])
         next_year = self._check_if_next_year(month, day)
         self.datetime_params["day"] = day
         self.datetime_params["month"] = month
         self.datetime_params["year"] += next_year
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _month_number_day_number(self, matches):
+    def _month_number_day_number(self, matches: List[str]) -> datetime:
         month, day = map(int, matches)
         next_year = self._check_if_next_year(month, day)
         self.datetime_params["day"] = day
         self.datetime_params["month"] = month
         self.datetime_params["year"] += next_year
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _month_number_day_number_year_number(self, matches):
+    def _month_number_day_number_year_number(self, matches: List[str]) -> datetime:
         month, day, year = map(int, matches)
         self.datetime_params["day"] = day
         self.datetime_params["month"] = month
         self.datetime_params["year"] = year
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _day_number_month_number(self, matches):
+    def _day_number_month_number(self, matches: List[str]) -> datetime:
         day, month = map(int, matches)
         next_year = self._check_if_next_year(month, day)
         self.datetime_params["day"] = day
         self.datetime_params["month"] = month
         self.datetime_params["year"] += next_year
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _day_number_month_number_year_number(self, matches):
+    def _day_number_month_number_year_number(self, matches: List[str]) -> datetime:
         day, month, year = map(int, matches)
         self.datetime_params["day"] = day
         self.datetime_params["month"] = month
         self.datetime_params["year"] = year
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _iso_date(self, matches):
+    def _iso_date(self, matches: List[str]) -> datetime:
         year, month, day = map(int, matches)
         self.datetime_params["day"] = day
         self.datetime_params["month"] = month
         self.datetime_params["year"] = year
-        return datetime(**self.datetime_params)
+        return self.date_time
 
-    def _next(self, matches):
-        inc_period = (
-            matches[0] if matches[0].endswith("s") else matches[0] + "s"
-        )
-        dt = datetime(**self.datetime_params)
-        ret = relativedelta(**{inc_period: 1})
+    def _next(self, matches: List[str]) -> datetime:
+        inc_period = matches[0] if matches[0].endswith("s") else matches[0] + "s"
+        print(inc_period)
+        dt = self.date_time
+        if inc_period == "years":
+            ret = relativedelta(years=1)
+        elif inc_period == "months":
+            ret = relativedelta(months=1)
+        elif inc_period == "weeks":
+            ret = relativedelta(weeks=1)
+        elif inc_period == "days":
+            ret = relativedelta(days=1)
+        elif inc_period == "hours":
+            ret = relativedelta(hours=1)
+        elif inc_period == "minutes":
+            ret = relativedelta(minutes=1)
         return dt + ret
 
-    def _inc_number(self, matches):
+    def _inc_number(self, matches: List[str]) -> datetime:
         inc_number = int(matches[0])
-        inc_period = (
-            matches[1] if matches[1].endswith("s") else matches[1] + "s"
-        )
-        return datetime(**self.datetime_params) + timedelta(
-            **{inc_period: inc_number}
+        inc_period = matches[1] if matches[1].endswith("s") else matches[1] + "s"
+        return self.date_time + timedelta(**{inc_period: inc_number})
+
+    def _check_if_next_day(self, hour: int, minute: int) -> int:
+        return int(
+            self.now.hour > hour or (self.now.hour == hour and self.now.minute > minute)
         )
 
-    def _check_if_next_day(self, hour, minute):
+    def _check_if_next_year(self, month: int, day: int) -> int:
         return int(
-            self.now.hour > hour
-            or (self.now.hour == hour and self.now.minute > minute)
-        )
-
-    def _check_if_next_year(self, month, day):
-        return int(
-            self.now.month > month
-            or (self.now.month == month and self.now.day > day)
+            self.now.month > month or (self.now.month == month and self.now.day > day)
         )
--- test/test_atdate.py
+++ test/test_atdate.py
@@ -5,246 +5,246 @@ from freezegun import freeze_time
 import atdate
 
 
-def test_at_date_has_parse_attribute():
-    assert hasattr(atdate, 'parse')
+def test_at_date_has_parse_attribute() -> None:
+    assert hasattr(atdate, "parse")
 
 
-def test_at_date_has_atdateparser_attribute():
-    assert hasattr(atdate, 'AtDateParser')
+def test_at_date_has_atdateparser_attribute() -> None:
+    assert hasattr(atdate, "AtDateParser")
 
 
-def test_parse_return_datetime_object():
-    test_string = 'noon'
+def test_parse_return_datetime_object() -> None:
+    test_string = "noon"
     result = atdate.parse(test_string)
     assert isinstance(result, datetime)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_noon_before_noon():
-    test_string = 'noon'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_noon_before_noon() -> None:
+    test_string = "noon"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 2, 12, 0, 0, 0)
 
 
-@freeze_time('2000-01-02 13:04:05')
-def test_at_noon_after_noon():
-    test_string = 'noon'
+@freeze_time("2000-01-02 13:04:05")
+def test_at_noon_after_noon() -> None:
+    test_string = "noon"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 3, 12, 0, 0, 0)
 
 
-@freeze_time('2000-01-31 13:04:05')
-def test_at_noon_month_change():
-    test_string = 'noon'
+@freeze_time("2000-01-31 13:04:05")
+def test_at_noon_month_change() -> None:
+    test_string = "noon"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 2, 1, 12, 0, 0, 0)
 
 
-@freeze_time('2000-12-31 13:04:05')
-def test_at_noon_year_change():
-    test_string = 'noon'
+@freeze_time("2000-12-31 13:04:05")
+def test_at_noon_year_change() -> None:
+    test_string = "noon"
     result = atdate.parse(test_string)
     assert result == datetime(2001, 1, 1, 12, 0, 0, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_midnight():
-    test_string = 'midnight'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_midnight() -> None:
+    test_string = "midnight"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 3, 0, 0, 0, 0)
 
 
-@freeze_time('2000-01-31 13:04:05')
-def test_at_midnight_month_change():
-    test_string = 'midnight'
+@freeze_time("2000-01-31 13:04:05")
+def test_at_midnight_month_change() -> None:
+    test_string = "midnight"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 2, 1, 0, 0, 0, 0)
 
 
-@freeze_time('2000-12-31 13:04:05')
-def test_at_midnight_year_change():
-    test_string = 'midnight'
+@freeze_time("2000-12-31 13:04:05")
+def test_at_midnight_year_change() -> None:
+    test_string = "midnight"
     result = atdate.parse(test_string)
     assert result == datetime(2001, 1, 1, 0, 0, 0, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_now():
-    test_string = 'now'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_now() -> None:
+    test_string = "now"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 2, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_now_next_minute_change_minute():
-    test_string = 'now next minute'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_now_next_minute_change_minute() -> None:
+    test_string = "now next minute"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 2, 3, 5, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_now_next_minutes():
-    test_string = 'now next minutes'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_now_next_minutes() -> None:
+    test_string = "now next minutes"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 2, 3, 5, 5, 0)
 
 
-@freeze_time('2000-01-02 03:59:05')
-def test_at_now_next_minute_change_hour():
-    test_string = 'now next minute'
+@freeze_time("2000-01-02 03:59:05")
+def test_at_now_next_minute_change_hour() -> None:
+    test_string = "now next minute"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 2, 4, 0, 5, 0)
 
 
-@freeze_time('2000-01-02 23:59:05')
-def test_at_now_next_minute_change_day():
-    test_string = 'now next minute'
+@freeze_time("2000-01-02 23:59:05")
+def test_at_now_next_minute_change_day() -> None:
+    test_string = "now next minute"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 3, 0, 0, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_now_next_hour():
-    test_string = 'now next hour'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_now_next_hour() -> None:
+    test_string = "now next hour"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 2, 4, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_now_next_day():
-    test_string = 'now next day'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_now_next_day() -> None:
+    test_string = "now next day"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 3, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_now_next_week():
-    test_string = 'now next week'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_now_next_week() -> None:
+    test_string = "now next week"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 1, 9, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_now_next_month():
-    test_string = 'now next month'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_now_next_month() -> None:
+    test_string = "now next month"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 2, 2, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_at_now_next_year():
-    test_string = 'now next year'
+@freeze_time("2000-01-02 03:04:05")
+def test_at_now_next_year() -> None:
+    test_string = "now next year"
     result = atdate.parse(test_string)
     assert result == datetime(2001, 1, 2, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_month_number_day_number():
-    test_string = '05/20'
+@freeze_time("2000-01-02 03:04:05")
+def test_month_number_day_number() -> None:
+    test_string = "05/20"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 5, 20, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_month_name_day_number():
-    test_string = 'May 20'
+@freeze_time("2000-01-02 03:04:05")
+def test_month_name_day_number() -> None:
+    test_string = "May 20"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 5, 20, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_month_number_day_number_year_number():
-    test_string = '05/20/2003'
+@freeze_time("2000-01-02 03:04:05")
+def test_month_number_day_number_year_number() -> None:
+    test_string = "05/20/2003"
     result = atdate.parse(test_string)
     assert result == datetime(2003, 5, 20, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_day_number_month_number():
-    test_string = '20.05'
+@freeze_time("2000-01-02 03:04:05")
+def test_day_number_month_number() -> None:
+    test_string = "20.05"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 5, 20, 3, 4, 5, 0)
 
 
-@freeze_time('2000-01-02 03:04:05')
-def test_day_number_month_number_year_number():
-    test_string = '20.05.2003'
+@freeze_time("2000-01-02 03:04:05")
+def test_day_number_month_number_year_number() -> None:
+    test_string = "20.05.2003"
     result = atdate.parse(test_string)
     assert result == datetime(2003, 5, 20, 3, 4, 5, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_inc_period():
-    test_string = '02.07.2000 +1days'
+@freeze_time("2000-07-02 03:04:05")
+def test_inc_period() -> None:
+    test_string = "02.07.2000 +1days"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 7, 3, 3, 4, 5, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_hr24clock_hr_min():
-    test_string = '1401'
+@freeze_time("2000-07-02 03:04:05")
+def test_hr24clock_hr_min() -> None:
+    test_string = "1401"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 7, 2, 14, 1, 0, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_hr24clock_hour_minute():
-    test_string = '14:01'
+@freeze_time("2000-07-02 03:04:05")
+def test_hr24clock_hour_minute() -> None:
+    test_string = "14:01"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 7, 2, 14, 1, 0, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_wallclock_hr_min_am_pm():
-    test_string = '0201 pm'
+@freeze_time("2000-07-02 03:04:05")
+def test_wallclock_hr_min_am_pm() -> None:
+    test_string = "0201 pm"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 7, 2, 14, 1, 0, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_wallclock_hour_minute_am_pm():
-    test_string = '02:01 pm'
+@freeze_time("2000-07-02 03:04:05")
+def test_wallclock_hour_minute_am_pm() -> None:
+    test_string = "02:01 pm"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 7, 2, 14, 1, 0, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_next_month_without_now():
-    test_string = 'next month'
+@freeze_time("2000-07-02 03:04:05")
+def test_next_month_without_now() -> None:
+    test_string = "next month"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 8, 2, 3, 4, 5, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_plus_one_day_without_now():
-    test_string = '+1days'
+@freeze_time("2000-07-02 03:04:05")
+def test_plus_one_day_without_now() -> None:
+    test_string = "+1days"
     result = atdate.parse(test_string)
     assert result == datetime(2000, 7, 3, 3, 4, 5, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_isodate():
-    test_string = '2011-09-22'
+@freeze_time("2000-07-02 03:04:05")
+def test_isodate() -> None:
+    test_string = "2011-09-22"
     result = atdate.parse(test_string)
     assert result == datetime(2011, 9, 22, 3, 4, 5, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_time_date():
+@freeze_time("2000-07-02 03:04:05")
+def test_time_date() -> None:
     test_string = "12:24 01.02.2011"
     result = atdate.parse(test_string)
     assert result == datetime(2011, 2, 1, 12, 24, 0, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_isodatetime():
-    test_string = '2011-09-22T11:44'
+@freeze_time("2000-07-02 03:04:05")
+def test_isodatetime() -> None:
+    test_string = "2011-09-22T11:44"
     result = atdate.parse(test_string)
     assert result == datetime(2011, 9, 22, 11, 44, 0, 0)
 
 
-@freeze_time('2000-07-02 03:04:05')
-def test_isodatetime_without_t():
-    test_string = '2011-09-22 11:44'
+@freeze_time("2000-07-02 03:04:05")
+def test_isodatetime_without_t() -> None:
+    test_string = "2011-09-22 11:44"
     result = atdate.parse(test_string)
     assert result == datetime(2011, 9, 22, 11, 44, 0, 0)
